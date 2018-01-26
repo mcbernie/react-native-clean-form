cdfd6fc508c103890e809e7a89bce6e6b8d23a99
Nicolas Brüggemann <nbrueggemann@itcoops.de>
2018-01-26 12:48:49 +0100

    scrollview

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ScrollView, View, KeyboardAvoidingView } from 'react-native'
+import { ScrollView, View, KeyboardAvoidingView, Platform } from 'react-native'
 import styled from 'styled-components/native'
 
 // Flex: 1 will force the form to take up remaining height of the view
@@ -24,7 +24,7 @@ class Form extends Component {
     const { children, ...rest } = this.props
 
     return (
-      <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={50} onLayout={this.onLayout} style={{ flex: 1 }} >
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "position"} keyboardVerticalOffset={50} onLayout={this.onLayout} style={{ flex: 1 }} >
         <ScrollView 
           contentContainerStyle={{ minHeight: this.state.height }} 
           >
